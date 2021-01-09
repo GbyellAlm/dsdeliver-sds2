c30f9e3220b5f5e36a1c22affcc92440e39bf1ea
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-01-09 12:03:52 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 // Armazenei essa url numa constante pq vamos usar ela em outros momentos tb
-const API_URL = 'http://localhost:8080'
+const API_URL= process.env.REACT_APP_API_URL;
 
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
